c95d8007237be0c13e4db1f2258406c323227782
jorgepalaciosc <jorgepalaciios91@gmail.com>
2023-07-13 23:43:50 -0400

    adding basic structure for task list app, styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 import './App.css';
-
+//COMPONENTS
+import Logo from './components/Logo';
 function App() {
   return (
     <div className="App">
-      
+      <Logo></Logo>
+      <div className="task-list-main">
+        <h1>My Tasks</h1>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+//FILES
+import logo from '../logo.svg'
+const Logo = () => (
+  <div className='logo-container'>
+    <img 
+    className='logo'
+    src={logo} 
+    alt="check logo" />
+  </div>
+)
+
+export default Logo
